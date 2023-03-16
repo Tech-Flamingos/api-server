3e6a61ceacb1bef125dec358b0c68b0aaf4f4645
MARTIN HANSEN <marrtt1234@gmail.com>
2023-03-16 17:53:37 +0000

    removed old code

--- src/auth/routes.js
+++ src/auth/routes.js
@@ -27,35 +27,7 @@ authRouter.post('/signup', async(req, res, next) => {
   }
 });
 
-authRouter.post('/testSignup', async(req, res, next) => {
-  try {
-    console.log(req.body);
-    const userRecord = new TestUserSchema(req.body);
-    await userRecord.save();
-    // let userRecord = await userModel.create(req.body);
-    const output = {
-      user: userRecord, 
-      token: userRecord.token,
-    };
-    res.status(201).json(output);
-
-  } catch (error) {
-    next(error.message);
-
-  }
-});
-
-
 authRouter.post('/signin', basicAuth, (req, res, next) => {
-  const user = {
-    user: req.user, 
-    token: req.user.token,
-  };
-  res.status(200).json(user);
-
-});
-
-authRouter.post('/testSignin', testBasicAuth, (req, res, next) => {
   const user = {
     user: req.user, 
     token: req.user.token,
