f3c7c2b90c0d6ec7bf713eda6acf3dd9155a3f62
Brenda <107290326+brenda70904@users.noreply.github.com>
2023-03-16 11:00:39 -0700

    Revert "Moke tests 2"

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-'use strict';
-// comment
-const mongoose = require('mongoose');
-require('dotenv').config();
-const app = require('./src/server');
-const port = process.env.PORT || 3002;
+// 'use strict';
+// // comment
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// const app = require('./src/server');
+// const port = process.env.PORT || 3002;
 
-mongoose.connect(process.env.DATABASE_URL)
-  .then(() => app.start(port));
+// mongoose.connect(process.env.DATABASE_URL)
+//   .then(() => app.start(port));
+
+'use strict';
+const server = require('./src/server');
+
+
+console.log('Starting server');
+server.start();
